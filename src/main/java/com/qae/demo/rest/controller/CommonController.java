4f6958b266f474d4b99b30bc04852088a311ac55
juney <juneyrang@gmail.com>
2023-08-20 18:56:06 +0900

    Default Changelist

--- src/main/java/com/qae/demo/rest/controller/CommonController.java
+++ src/main/java/com/qae/demo/rest/controller/CommonController.java
@@ -1,18 +1,14 @@
 package com.qae.demo.rest.controller;
 
 import com.qae.demo.dto.response.BaseInfoResponse;
-import com.qae.demo.dto.response.FileResponse;
 import com.qae.demo.rest.service.BaseInfoService;
-import com.qae.demo.rest.service.FileService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
 
 @Slf4j
 @Tag(name = "FileController", description = "File Upload 관련 api")
