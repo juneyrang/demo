fb4e2f0002c3d1f5b8ab35394750e0b0fa1f42c3
양준식 <62315680+juneyrang@users.noreply.github.com>
2023-09-06 14:16:06 +0900

    desm

--- src/main/webapp/test/js/app/system/menu.js
+++ src/main/webapp/test/js/app/system/menu.js
@@ -9,7 +9,31 @@
 		},
 		callback: function (data, textStatus, jqXHR) {
 			sidebarPageObj.setMenu(data.results);
-	    }
+	    },
+        getTreeData: function(data) {
+            var arrData = [];
+            $.each(data, function(i, row) {
+                if(row.MENU_UP_SEQ == '0') {
+                    var item = {
+                          'MENU_SEQ' 	: row['MENU_SEQ']
+                        , 'MENU_NAME' 	: row['MENU_NAME']
+                        , 'MENU_UP_SEQ' : row['MENU_UP_SEQ']
+                        , 'MENU_PATH' 	: row['MENU_PATH']
+                        , 'MENU_ORDER' 	: row['MENU_ORDER']
+                        , 'TRANS_YN' 	: row['TRANS_YN']
+                        , 'MOBILE_YN' 	: row['MOBILE_YN']
+                    };
+                    item._children = [];
+                    $.each(data, function(i, child) {
+                        if(child.MENU_UP_SEQ != '0' && row.MENU_SEQ == child.MENU_UP_SEQ) {
+                            item._children.push(child);
+                        }
+                    });
+                    arrData.push(item);
+                }
+            });
+            return arrData;
+        }
 	};
 	
 	menuContentObj.init();
